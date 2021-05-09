3a48ea67fbe4cd87cafe20cc78bd775af42897c5
Sean Deneen <sdeneen@tripadvisor.com>
2021-05-09 18:31:35 -0400

    Adds graph traversal, and Jest unit test framework

--- buildAndCopyToClipboard.sh
+++ buildAndCopyToClipboard.sh
@@ -1,3 +1,4 @@
 yarn install
 node_modules/rollup/dist/bin/rollup -c
+yarn test
 cat dist/bundle.js | pbcopy
--- src/model/Cell.ts
+++ src/model/Cell.ts
@@ -1,6 +1,12 @@
 export default class Cell {
     index: number;
     richness: number;
+
+    /**
+     * Indices of neighboring cells, one for each direction (6 directions for a hex board).
+     * Beware that some directions may have an index of -1, indicating that there is no neighbor
+     * in that direction (i.e. this cell is at the edge of the board)
+     */
     neighbors: [number, number, number, number, number, number];
 
     constructor(
@@ -12,4 +18,9 @@ export default class Cell {
         this.richness = richness;
         this.neighbors = neighbors;
     }
+
+    /**
+     * Get indices of all cells that neighbor this cell.
+     */
+    getNeighborCellIndices = () => this.neighbors.filter(neighborIndex => neighborIndex >= 0);
 }
