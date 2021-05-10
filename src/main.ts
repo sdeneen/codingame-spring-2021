f831e524d2c15672d188dbbed4a46060344dd77c
Mathurshan Vimalesvaran <mvimalesvaran@hubspot.com>
2021-05-09 22:57:52 -0400

    typescript yay

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,15 @@ import getActionForSunPointSaverStrategy from "./strategy/sunPointSaverStrategy"
 import getActionForCompleteTreesStrategy from "./strategy/completeTreesStrategy";
 import { NUM_TURNS } from "./miscConstants";
 import { getTrashTalk } from "./utils/trashTalker";
+import Action from './model/Action';
+import Cell from './model/Cell';
+import Game from './model/Game';
 
-const cells = parseInitializationInput();
+const cells: Cell[] = parseInitializationInput();
 
 while (true) {
-    const game = parseTurnInput(cells);
-    let action;
+    const game: Game = parseTurnInput(cells);
+    let action: Action;
     if (game.day / NUM_TURNS >= 0.75) {
         action = getActionForCompleteTreesStrategy(game);
     } else {
