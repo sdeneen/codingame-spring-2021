7194c97cd98c2a86a3a9b74d15e91f30e671436d
Sean Deneen <sdeneen@tripadvisor.com>
2021-05-15 00:27:03 -0400

    trash talkin

--- src/utils/trashTalker.ts
+++ src/utils/trashTalker.ts
@@ -2,6 +2,9 @@ const SMACK = [
   "Get got!",
   "That's cute.",
   "Only spooky thing around here is your algo.",
+  "Your tree's only 3 inches long heheh.",
+  "Your jokes are pretty sappy",
+  "Time for you to leaf",
 ];
 
 const getTrashTalk = (): string => {
